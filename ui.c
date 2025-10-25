08801115bb98a67b1700d126e2234623bb9e7bf5
jcleon07 <jcleon0722@gmail.com>
2025-10-25 17:10:39 -0500

    Creacion Makefile y UI

---
+++ ui.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "hash.h"
+
+int main() {
+    // Implementación de la interfaz de usuario
+    printf("Bienvenido.\n");
+ 
+ 
+ 
+    return 0;
+}
