297ac58d94faca167e13036fec2983f11f2575b5
jcleon07 <jcleon0722@gmail.com>
2025-10-25 17:18:47 -0500

    Configuracion cliente en UI

--- ui.c
+++ ui.c
@@ -6,11 +6,38 @@
 #include <netinet/in.h>
 #include "hash.h"
 
+#define PORT 3539
+#define BACKLOG 4 
+
 int main() {
-    // Implementación de la interfaz de usuario
+    
     printf("Bienvenido.\n");
  
- 
+    //Declaracion de variables
+    int fd, r;
+    struct sockaddr_in server, cliente;
+    char buffer[20];
+
+    //Creacion del socket
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (fd < 0) {
+            perror("Error al crear el socket");
+            exit(1);
+        }
+
+    //Configuracion del servidor
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(server.sin_zero),8);
+    
+    //Coneccion al servidor
+    r = connect(fd, (struct sockaddr *)&server, sizeof(struct sockaddr_in));
+        if (r < 0) {
+            perror("Error en connect");
+            close(fd);
+            exit(1);
+        }
  
     return 0;
 }
